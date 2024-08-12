41180b30ff43d74b36ee84f115b80ebf5b8d60c9
Jony <jonybhattacharjee886@gmail.com>
2024-08-13 05:25:57 +0600

    Update plate

--- server/src/controller/plate.controller.ts
+++ server/src/controller/plate.controller.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 import { prisma } from "../utils/db";
 
 export const getPlateDetailsById = async (req: Request, res: Response) => {
-  const plate = req.params.plate;
+  const plate = req.params.plate.replace(/\s+/g, "");
+  // console.log(plate);
 
   try {
     // Query the database using the plate number
@@ -12,13 +13,13 @@ export const getPlateDetailsById = async (req: Request, res: Response) => {
 
     // If no vehicle found, send a 404 response
     if (!vehicleDetails) {
-      return res.status(404).send("Vehicle not found");
+      return res.status(404).json("Vehicle not found");
     }
 
     // Send the vehicle details as the response
     res.status(200).json(vehicleDetails);
   } catch (error) {
     console.log(error);
-    res.status(500).send("Error retrieving vehicle details");
+    res.status(500).json("Error retrieving vehicle details");
   }
 };
