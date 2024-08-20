2158ee3790c2eb8b772646811d951514cb5c7a26
Jony <jonybhattacharjee886@gmail.com>
2024-08-21 02:09:11 +0600

    update baseurl to onrender

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const baseUrl = "https://shortcover.co/";
+export const baseUrl = "https://shortcover.onrender.com/";
